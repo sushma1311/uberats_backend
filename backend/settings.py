d4248c7f4748a6b7543221b1ef95ffafeb54284e
Sushma Tacholi Kudai <sushma.tacholikudai@sjsu.edu>
2024-10-28 13:23:35 -0700

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -135,3 +135,6 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# Enable serving static and media files via WhiteNoise
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
